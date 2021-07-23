4b4e2e4053caba2d463258e1ddacae89a159ff86
nativedone <adeildoamorim@gmail.com>
2021-07-23 08:55:01 -0300

    test notifications

--- main.js
+++ main.js
@@ -1,28 +1,86 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const electron = require("electron");
 
-const { setup: setupPushReceiver } = require('electron-push-receiver');
+const app = electron.app;
+const Menu = electron.Menu;
+const nativeImage = electron.nativeImage;
+const Tray = electron.Tray;
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+let tray = null;
+let mainWindow;
+let isQuitting;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const { setup: setupPushReceiver } = require("electron-push-receiver");
 
-function createWindow () {
+const path = require("path");
+const url = require("url");
+
+const exeName = path.basename(process.execPath);
+app.setLoginItemSettings({
+  openAtLogin: true,
+  path: process.execPath,
+  args: ["--processStart", `${exeName}`, "--process-start-args", "--hidden"],
+});
+
+app.on("before-quit", function () {
+  isQuitting = true;
+});
+
+const icon = path.join(__dirname, "/assets/home-smile-fill.png"); // required.
+console.log(`path.join(__dirname, "")`, path.join(__dirname, ""));
+function createTray() {
+  const trayIcon = nativeImage.createFromPath(icon);
+  tray = new Tray(trayIcon.resize({ width: 16 }));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: `Abrir aplicativo`,
+      click: () => {
+        if (app.dock) {
+          app.dock.show();
+        }
+        if (mainWindow.setSkipTaskbar) {
+          mainWindow.setSkipTaskbar(false);
+        }
+        mainWindow.show();
+      },
+    },
+    {
+      label: `View DEV tools`,
+      click: () => {
+        mainWindow.openDevTools();
+      },
+    },
+    {
+      type: "separator",
+    },
+
+    {
+      label: `Fechar`,
+      click: () => {
+        isQuitting = true;
+        app.quit(); // actually quit the app.
+      },
+    },
+  ]);
+  tray.setContextMenu(contextMenu);
+}
+
+function createWindow() {
+  if (!tray) {
+    // if tray hasn't been created already.
+    createTray();
+  }
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 
   // Initialize electron-push-receiver component. Should be called before 'did-finish-load'
   setupPushReceiver(mainWindow.webContents);
@@ -30,36 +88,51 @@ function createWindow () {
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+  // // Emitted when the window is closed.
+  // mainWindow.on("closed", function () {
+  //   // Dereference the window object, usually you would store windows
+  //   // in an array if your app supports multi windows, this is the time
+  //   // when you should delete the corresponding element.
+  //   mainWindow = null;
+  // });
+
+  mainWindow.on("close", function (event) {
+    if (!isQuitting) {
+      event.preventDefault();
+      mainWindow.hide();
+      if (app.dock) {
+        app.dock.hide();
+      }
+      if (mainWindow.setSkipTaskbar) {
+        mainWindow.setSkipTaskbar(true);
+      }
+
+      event.returnValue = false;
+    }
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+// app.on("window-all-closed", function () {
+//   // On OS X it is common for applications and their menu bar
+//   // to stay active until the user quits explicitly with Cmd + Q
+//   if (process.platform !== "darwin") {
+//     app.quit();
+//   }
+// });
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -2,53 +2,65 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const { ipcRenderer } = require ('electron')
+require("dotenv").config();
+
+const { ipcRenderer } = require("electron");
 const {
   START_NOTIFICATION_SERVICE,
   NOTIFICATION_SERVICE_STARTED,
   NOTIFICATION_SERVICE_ERROR,
   NOTIFICATION_RECEIVED,
   TOKEN_UPDATED,
-} = require ('electron-push-receiver/src/constants')
+} = require("electron-push-receiver/src/constants");
 
 // Listen for service successfully started
 ipcRenderer.on(NOTIFICATION_SERVICE_STARTED, (_, token) => {
-  console.log('service successfully started', token)
-})
+  console.log("service successfully started", token);
+  var elem = document.getElementById("fcm-token");
+  elem.style.color = "blue";
+  elem.innerHTML = token;
+});
 
 // Handle notification errors
 ipcRenderer.on(NOTIFICATION_SERVICE_ERROR, (_, error) => {
-  console.log('notification error', error)
-})
+  console.log("notification error", error);
+});
 
 // Send FCM token to backend
 ipcRenderer.on(TOKEN_UPDATED, (_, token) => {
-  console.log('token updated', token)
-})
+  console.log("token updated", token);
+  var elem = document.getElementById("fcm-token");
+  elem.style.color = "blue";
+  elem.innerHTML = token;
+});
 
 // Display notification
 ipcRenderer.on(NOTIFICATION_RECEIVED, (_, serverNotificationPayload) => {
   // check to see if payload contains a body string, if it doesn't consider it a silent push
-  if (serverNotificationPayload.notification.body){
+  if (serverNotificationPayload.notification.body) {
     // payload has a body, so show it to the user
-    console.log('display notification', serverNotificationPayload)
-    let myNotification = new Notification(serverNotificationPayload.notification.title, {
-      body: serverNotificationPayload.notification.body
-    })
-    
+    console.log("display notification", serverNotificationPayload);
+    let myNotification = new Notification(
+      serverNotificationPayload.notification.title,
+      {
+        body: serverNotificationPayload.notification.body,
+      }
+    );
+
     myNotification.onclick = () => {
-      console.log('Notification clicked')
-    }  
+      console.log("Notification clicked");
+    };
   } else {
     // payload has no body, so consider it silent (and just consider the data portion)
-    console.log('do something with the key/value pairs in the data', serverNotificationPayload.data)
+    console.log(
+      "do something with the key/value pairs in the data",
+      serverNotificationPayload.data
+    );
   }
-})
+});
 
 // Start service
-const senderId = '123456789' // <-- replace with FCM sender ID from FCM web admin under Settings->Cloud Messaging
-console.log('starting service and registering a client')
-ipcRenderer.send(START_NOTIFICATION_SERVICE, senderId)
-
-
-
+const senderId = process.env.SENDER_ID; // <-- replace with FCM sender ID from FCM web admin under Settings->Cloud Messaging
+console.log("starting service and registering a client");
+console.log("process.env.SENDER_ID", JSON.stringify(process.env, null, 2));
+ipcRenderer.send(START_NOTIFICATION_SERVICE, senderId);
